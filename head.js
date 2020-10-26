fe07d3a20c676ab0e78b8b3dc230b150503c5dcb
Jennifer Jung <jenjung2194@gmail.com>
2020-10-26 21:14:14 +0000

    added tail.js

--- head.js
+++ head.js
@@ -4,10 +4,10 @@ const head = (arr) => {
 
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
-
-  console.assert(actual === expected, `${actual} !== ${expected}`);
 };
 
 assertEqual(head([5, 6, 7]), 5);
