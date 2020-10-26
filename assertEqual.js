297ce091863a6ecfe6f99d90e7f594b6aac1b008
Jennifer Jung <jenjung2194@gmail.com>
2020-10-26 21:12:43 +0000

    added emojis and got rid of console.assert. used console.log instead

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
-
-  console.assert(actual === expected, `${actual} !== ${expected}`);
 };
 
 assertEqual("Lighthouse Labs", "Bootcamp");
