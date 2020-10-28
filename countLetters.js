fe58b5edd1311c6e3ba217b4db0e5ca8f9d7f305
Jennifer Jung <jenjung2194@gmail.com>
2020-10-28 16:23:23 +0000

    countLetter function returning object

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//PLAN
+//
+const countLetters = (str) => {
+  const noSpace = str.slice().split(" ").join("");
+  let result = {};
+
+  for (const letter of noSpace) {
+    if (result[letter]) {
+      result[letter]++;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse labs"));
