b8d54f8ef3aad06249e0a6660af08c00fae47e7e
Jennifer Jung <jenjung2194@gmail.com>
2020-10-30 21:20:23 +0000

    used recursion to compare objects as well as arrays

--- eqObjects.js
+++ eqObjects.js
@@ -26,7 +26,9 @@ const eqObjects = (obj1, obj2) => {
         return false;
       }
     } else {
-      if (obj1[key] !== obj2[key]) {
+      if (typeof obj1[key] === "object") {
+        if (!eqObjects(obj1[key], obj2[key])) return false;
+      } else if (obj1[key] !== obj2[key]) {
         return false;
       }
     }
@@ -53,3 +55,13 @@ assertEqual(eqObjects(cd, dc), true);
 const cd2 = { c: "1", d: ["2", 3, 4] };
 eqObjects(cd, cd2); // => false
 assertEqual(eqObjects(cd, cd2), false);
+
+
+//stretch exercise
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { y: 0, z: 2 }, b: 2 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { y: 0, z: 1 }, b: 3 })) // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { y: 0, z: 1 }, b: 2 })) // => true
