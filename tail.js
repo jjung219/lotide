d16dcee6297131ed1d3a1935b93db9d42f108b70
Jennifer Jung <jenjung2194@gmail.com>
2020-11-02 18:07:38 +0000

    separated tail.js and  tailTest.js

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = (arr) => {
   let newArr = [];
   for (let i = 1; i < arr.length; i++) {
@@ -15,15 +7,4 @@ const tail = (arr) => {
   return newArr;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const otherResult = tail([1]);
-assertEqual(otherResult.length, 0);
+module.exports = tail;
