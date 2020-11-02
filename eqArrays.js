933925f118071ce1b5e773ccfee8cc5ec3f8e317
Jennifer Jung <jenjung2194@gmail.com>
2020-11-02 18:21:38 +0000

    Refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,16 +1,3 @@
-const assertEqual = (actual, expected) => {
-  // go through the values one by on
-  // compare the values at the same index
-  // if they are the same, move on. If they aren't the same, stop the loop
-  // print out false if the loop was stopped, true if the loop kept going
-
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (arrOne, arrTwo) => {
   if (arrOne.length !== arrTwo.length) return false;
 
@@ -26,18 +13,4 @@ const eqArrays = (arrOne, arrTwo) => {
   return true;
 };
 
-// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
-
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-
-//Stretch - recursive eqArrays
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])) // => true
-
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])) // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])) // => false
-console.log(eqArrays([[2, 3, 7], 4], [[2, 3], 4])) // => false
-console.log(eqArrays([[2, 3], [4, 6, 7]], [[2, 3], [4, 6, 7]])) // => true
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,18 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+//Stretch - recursive eqArrays
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[2, 3, 7], 4], [[2, 3], 4]), false);
+assertEqual(eqArrays([[2, 3], [4, 6, 7]], [[2, 3], [4, 6, 7]]), true);
