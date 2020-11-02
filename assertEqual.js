337a8d8f3298982897e3df18cfc940ba751bca23
Jennifer Jung <jenjung2194@gmail.com>
2020-11-02 18:04:17 +0000

    separated assertEqual & head with assertEqualTest and headTest

--- assertEqual.js
+++ assertEqual.js
@@ -4,5 +4,4 @@ const assertEqual = (actual, expected) => {
     : console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -2,14 +2,6 @@ const head = (arr) => {
   return arr[0];
 };
 
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+module.exports = head;
